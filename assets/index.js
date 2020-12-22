25760a1e564e474c846c441d7cda1cdfcbeca004
Zach Turner <zakkturner@yahoo.com>
2020-12-22 13:30:56 -0500

    v2 of animation

--- assets/index.js
+++ assets/index.js
@@ -4,21 +4,30 @@ let rightBar = document.getElementById('rightBar');
 let circle = document.getElementById('circle');
 let header = document.getElementById('shopify-section-header');
 let homeMask = document.getElementById('homeMask');
+let text = document.getElementById('introTxt');
 
-gsap.registerPlugin(DrawSVGPlugin)
+gsap.registerPlugin(DrawSVGPlugin, SplitText)
 
-let tl = gsap.timeline()
+var SplitText = new SplitText(text, {type: "words,chars"});
+var chars = SplitText.chars;
+var words = SplitText.words;
+
+
+let tl = gsap.timeline();
 function init(){
-    
     tl.set(document.body, {overflow: "hidden"})
     tl.set(circle, {autoAlpha: 1})
+    tl.set(text, {autoAlpha: 1})
     tl.from(logo, .8, { delay: .5, opacity: 0, autoAlpha: 0, y: 100, ease: "power3.inOut"}, 'logo');
-    // tl.to(circle, { delay:.5, duration: .1, autoAlpha: 1})
     tl.fromTo(circle, 1, {drawSVG: 0 },{  drawSVG: '100%', autoAlpha: 1,   delay: .7, ease: "circ.inOut"},'logo');
     
-    tl.to([logo, circle], .5, { opacity: 0, delay: 3.5, stagger: .2}, 'logo')
-    tl.to( leftBar, 1, { delay: -.3, x: '-100%',  ease: "expo.inOut"}, 'bars')
-    tl.to( rightBar, 1, { delay: -.3, x: '100%',  ease: "expo.inOut"} , 'bars')
+    tl.to( circle, .5, { opacity: 0 }, 'text')
+    tl.from(chars, .5, {x:-200, y:100, opacity: 0, ease: "expo.inOut", stagger: .1}, 'text')
+
+    tl.to(logo, .8, {x: -200, scale: .5,  ease: "expo.inOut"}, 'text')
+    tl.to([logo, words], {y: 200, opacity: 0, delay: 2, stagger: .2, ease: "power3.inOut"}, "text")
+    tl.to( leftBar, 1, { delay: .5, x: '-100%',  ease: "expo.inOut"}, 'bars')
+    tl.to( rightBar, 1, { delay: .5, x: '100%',  ease: "expo.inOut"} , 'bars')
     tl.to(homeMask, .1,{delay: 1, css:{display: "none"}}, 'bars')
     tl.fromTo(header, { delay: 2, y: -200, autoAlpha: 1, display: "none"}, { y: 0, display: "block", ease: "power3.inOut"}, 'bars')
     tl.set(document.body, { delay: 1, overflow: "auto"}, 'bars')
