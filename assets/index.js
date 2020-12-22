84f42803dbad0e5036d19b3ff8c6326ecd792446
Zach Turner <zakkturner@yahoo.com>
2020-12-22 10:57:48 -0500

    basic animation done

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,31 @@
+let logo = document.getElementById('anLogo');
 let leftBar = document.getElementById('leftBar');
 let rightBar = document.getElementById('rightBar');
+let circle = document.getElementById('circle');
+let header = document.getElementById('shopify-section-header');
+let homeMask = document.getElementById('homeMask');
 
-// gsap.to( leftBar, .5, { delay: 5, x: '-100%'})
-// gsap.to( rightBar, .5, { delay: 5, x: '100%'})
+gsap.registerPlugin(DrawSVGPlugin)
+
+let tl = gsap.timeline()
+function init(){
+    
+    tl.set(document.body, {overflow: "hidden"})
+    tl.set(circle, {autoAlpha: 1})
+    tl.from(logo, .8, { delay: .5, opacity: 0, autoAlpha: 0, y: 100, ease: "power3.inOut"}, 'logo');
+    // tl.to(circle, { delay:.5, duration: .1, autoAlpha: 1})
+    tl.fromTo(circle, 1, {drawSVG: 0 },{  drawSVG: '100%', autoAlpha: 1,   delay: .7, ease: "circ.inOut"},'logo');
+    
+    tl.to([logo, circle], .5, { opacity: 0, delay: 3.5, stagger: .2}, 'logo')
+    tl.to( leftBar, 1, { delay: -.3, x: '-100%',  ease: "expo.inOut"}, 'bars')
+    tl.to( rightBar, 1, { delay: -.3, x: '100%',  ease: "expo.inOut"} , 'bars')
+    tl.to(homeMask, .1,{delay: 1, css:{display: "none"}}, 'bars')
+    tl.fromTo(header, { delay: 2, y: -200, autoAlpha: 1, display: "none"}, { y: 0, display: "block", ease: "power3.inOut"}, 'bars')
+    tl.set(document.body, { delay: 1, overflow: "auto"}, 'bars')
+    
+
+}
+
+window.addEventListener("load", function(event){
+    init();
+})
