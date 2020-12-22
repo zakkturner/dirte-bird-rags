af796716ca3a1745e51fbdd009843ee3a30d5364
Zach Turner <zakkturner@yahoo.com>
2020-12-22 15:45:48 -0500

    animation done

--- assets/index.js
+++ assets/index.js
@@ -19,17 +19,17 @@ function init(){
     tl.set(circle, {autoAlpha: 1})
     tl.set(text, {autoAlpha: 1})
     tl.from(logo, .8, { delay: .5, opacity: 0, autoAlpha: 0, y: 100, ease: "power3.inOut"}, 'logo');
-    tl.fromTo(circle, 1, {drawSVG: 0 },{  drawSVG: '100%', autoAlpha: 1,   delay: .7, ease: "circ.inOut"},'logo');
+    tl.fromTo(circle, 1, {drawSVG: 0 },{  drawSVG: '100%', autoAlpha: 1,   delay: 1, ease: "circ.inOut"},'logo');
     
     tl.to( circle, .5, { opacity: 0 }, 'text')
     tl.from(chars, .5, {x:-200, y:100, opacity: 0, ease: "expo.inOut", stagger: .1}, 'text')
 
     tl.to(logo, .8, {x: -200, scale: .5,  ease: "expo.inOut"}, 'text')
-    tl.to([logo, words], {y: 200, opacity: 0, delay: 2, stagger: .2, ease: "power3.inOut"}, "text")
-    tl.to( leftBar, 1, { delay: .5, x: '-100%',  ease: "expo.inOut"}, 'bars')
-    tl.to( rightBar, 1, { delay: .5, x: '100%',  ease: "expo.inOut"} , 'bars')
+    tl.to([logo, words], {y: 200, opacity: 0, delay: 2, stagger: .1, ease: "power3.inOut"}, "text")
+    tl.to( leftBar, 1, {  x: '-100%',  ease: "expo.inOut"}, 'bars')
+    tl.to( rightBar, 1, {  x: '100%',  ease: "expo.inOut"} , 'bars')
     tl.to(homeMask, .1,{delay: 1, css:{display: "none"}}, 'bars')
-    tl.fromTo(header, { delay: 2, y: -200, autoAlpha: 1, display: "none"}, { y: 0, display: "block", ease: "power3.inOut"}, 'bars')
+    tl.fromTo(header, { delay: 2, y: -200, autoAlpha: 1, display: "none"}, { y: 0, duration: 1.5, display: "block", ease: "power3.inOut"}, 'bars')
     tl.set(document.body, { delay: 1, overflow: "auto"}, 'bars')
     
 
